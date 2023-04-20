8c2055599247a6a9a374f39d4f0bf39df797d11c
Łukasz Tomków <lukasz.tomkow97@gmail.com>
2023-04-20 20:27:12 +0200

    first ocr test

--- image_read.py
+++ image_read.py
@@ -1,3 +1,7 @@
 import pytesseract
-from PIL import Image
-#test
+import PIL.Image
+import cv2
+
+myconfig = r"--psm 6 --oem 3"
+text = pytesseract.image_to_string(PIL.Image.open("Przechwytywanie.PNG"), config = myconfig)
+print(text)
