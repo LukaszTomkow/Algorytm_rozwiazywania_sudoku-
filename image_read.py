9a7ee6f3d14c1dc987b4cb791afc81745da40bb2
Łukasz Tomków <lukasz.tomkow97@gmail.com>
2023-05-24 16:28:39 +0200

    image processing upgrade and  grid extraction

--- image_read.py
+++ image_read.py
@@ -1,41 +1,74 @@
 import pytesseract
 import PIL.Image
 import cv2
+import numpy as np
 
-image_name = "sudoku.jpg"
+def extract_sudoku_cells(sudoku_grid):
+    cell_height = sudoku_grid.shape[0] // 9
+    cell_width = sudoku_grid.shape[1] // 9
+    cells = []
+    for row in range(9):
+        for col in range(9):
+            cell = sudoku_grid[row*cell_height:(row+1)*cell_height, col*cell_width:(col+1)*cell_width]
+            cells.append(cell)
+    return cells
 
+def preprocess_digit(digit):
+    # Progowanie adaptacyjne
+    thresh = cv2.adaptiveThreshold(digit, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+
+    # Wyostrzenie krawędzi
+    kernel = np.array([[0, -1, 0], [-1, 5,-1], [0, -1, 0]], dtype=np.float32)
+    sharpened = cv2.filter2D(thresh, -1, kernel)
+
+    return sharpened
+
+image_name = "przechwytywanie.png"
 
 # Przygotowanie obrazka
 img = cv2.imread(image_name)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-img = cv2.imread(image_name)
-basewidth = 600
-tempimg = PIL.Image.open(image_name)
-wpercent = (basewidth/float(tempimg.size[0]))
-hsize = int((float(tempimg.size[1])*float(wpercent)))
-tempimg = tempimg.resize((basewidth,hsize), PIL.Image.Resampling.LANCZOS)
-tempimg.save(image_name)
-height, width, _ = img.shape
 
+# Wyodrębnienie siatki Sudoku
+edges = cv2.Canny(thresh, 50, 150)
+contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contour = max(contours, key=cv2.contourArea)
+epsilon = 0.05 * cv2.arcLength(contour, True)
+approx = cv2.approxPolyDP(contour, epsilon, True)
+x, y, w, h = cv2.boundingRect(approx)
+sudoku_grid = thresh[y:y+h, x:x+w]
 
-#boxes = pytesseract.image_to_boxes(img, config = myconfig)
-#for box in boxes.splitlines():
-#    box = box.split(" ")
-#    img = cv2.rectangle(img, (int(box[1]), height - int(box[2])), (int(box[3]), height - int(box[4])),(0,255,0), 2  )
-#cv2.imshow("img",img)
-#cv2.waitKey(0)
-
+# Wyodrębnienie komórek Sudoku
+cells = extract_sudoku_cells(sudoku_grid)
 
 # Ustawienia Tesseract
 myconfig = r"--psm 6 --oem 3"
-# Wykonanie OCR na przetworzonym obrazku
-text = pytesseract.image_to_string(PIL.Image.open(image_name), config = myconfig)
-print(text)
+
+# Przetwarzanie komórek i wykonywanie OCR
+extracted_text = []
+for cell in cells:
+
+    cv2.imshow("Digit", cell)
+    cv2.waitKey(0)
+
+    digit = preprocess_digit(cell)
+    text = pytesseract.image_to_string(PIL.Image.fromarray(digit), config=myconfig)
+    print(text)
+    extracted_text.append(text.strip())
 
 # Przekształcenie tekstu
-processed_text = ''.join([c for c in text if c.isdigit()])  # Usunięcie wszystkich znaków, oprócz cyfr
+processed_text = ''
+for text in extracted_text:
+    if text.isdigit():
+        processed_text += text
+    else:
+        processed_text += ' '
 
-
-print(processed_text)
+# Sprawdzenie, czy otrzymany tekst składa się z 81 cyfr
+if len(processed_text) == 81:
+    sudoku_board = np.array([int(digit) if digit.isdigit() else 0 for digit in processed_text]).reshape((9, 9))
+    print(sudoku_board)
+else:
+    print("Błąd: Nieprawidłowa liczba cyfr Sudoku")
